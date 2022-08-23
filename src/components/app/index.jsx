7d427992cd1f941b018b2d60918b5082e391b954
dudubr1441 <masterdudu123@hotmail.com>
2022-08-23 10:53:52 -0300

    version 0.1.2

--- src/components/app/index.jsx
+++ src/components/app/index.jsx
@@ -3,7 +3,18 @@ import './App.css';
 function App() {
   return (
     <div id='content_pokemon'>
-
+        <div id="pokemonButtonsTop" className="pokemonButtons">
+                <div id="pokemonButtonsTopContent">
+                    <span className="pokedexCircle circleLow">
+                    </span>
+                    <span className="pokedexCircle circleLow"></span>
+                </div>
+            </div>
+            <div id="screenDiv">
+              <div id='screen'>
+              </div>
+            </div>
+          <footer id="pokemonButtonsLow" className="pokemonButtons"></footer>
     </div>
   );
 }
---
+++ src/private/pokedexFormat.js
@@ -0,0 +1,9 @@
+export const pokedex  = require('./files/pokedex.json').map((pokemon)=>{
+    return {name:pokemon.name.english
+            ,type:pokemon.type
+            ,abilities:[pokemon.profile.ability[0],pokemon.profile.ability[1]]
+            ,bigImage:pokemon.image.hires
+            ,mediumImage:pokemon.image.thumbnail
+            ,smallImage:pokemon.image.sprite
+            ,species:pokemon.species.replace('Pokémon','').trim()}
+});
