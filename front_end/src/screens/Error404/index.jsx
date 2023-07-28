9128c6df7b01ea7b034883112aa615449a082a1a
luiz-eduardo14 <masterdudu123@hotmail.com>
2023-07-28 20:03:16 -0300

    feat: apply pokemon background and remove text "Not Found" for include start button

--- front_end/src/screens/Error404/index.jsx
+++ front_end/src/screens/Error404/index.jsx
@@ -1,10 +1,16 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 import './style.css'
 function Error404() {
+  const navigate = useNavigate();
     return(
-        <div id='screen'>
+        <div id='screen' className="start" style={{
+          display:'flex',
+          flexDirection:'column',
+          justifyContent:'center',
+          alignItems:'center',
+        }}>
           <h1 className="namePokemon" style={{textAlign:"center",color:'#FF0000'}}><h1 style={{color:'#00FF00'}}>404</h1> Not Found</h1>
-          <span id='buttonToHome'><Link style={{textDecoration:'none',color:'#000000',userSelect:'none',fontSize:'40px'}} to={'/'}>go to home</Link></span>
+          <img onClick={()=> navigate('/home')} id='startImage' src='http://localhost:5000/images/home/pokemonButtonStart.png' height={80} width={80}></img>
         </div>
     );
 }
